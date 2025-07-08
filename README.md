0b8b448c3e251499242889897711f9cec9ff19d4
A-mistake-hello <3329989998@qq.com>
2025-07-08 22:35:42 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+# Android device tree for vivo V2219A (PD2219)-[vivo Y77]
+
+```
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
+
+#TWRP_for_vivo_Y77_Image_[UNOFFICIAL]_By A-mistake-hello(XingKongTanSuo)
+
+```
+#Team Win Recovery Rroject
+```
---
+++ omni_PD2219.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from PD2219 device
+$(call inherit-product, device/vivo/PD2219/device.mk)
+
+PRODUCT_DEVICE := PD2219
+PRODUCT_NAME := omni_PD2219
+PRODUCT_BRAND := vivo
+PRODUCT_MODEL := V2219A
+PRODUCT_MANUFACTURER := vivo
+
+PRODUCT_GMS_CLIENTID_BASE := android-vivo
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="vext_k6855v1_64-user 12 SP1A.210812.003 compiler01031104 release-keys"
+
+BUILD_FINGERPRINT := vivo/PD2219/PD2219:12/SP1A.210812.003/compiler01031104:user/release-keys
